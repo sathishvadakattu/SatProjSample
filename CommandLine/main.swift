9eb8a79fb8e95baae057b8cd5dbdb4232c6561a2
Sathish Vadakattu <sathish.vadakattu@solugenix.com>
2020-06-23 16:46:23 +0530

    Initial Commit

---
+++ CommandLine/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CommandLine
+//
+//  Created by Solugenix India on 23/06/20.
+//  Copyright © 2020 Solugenix India. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
